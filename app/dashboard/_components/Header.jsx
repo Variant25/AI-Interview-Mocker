5dd37d747c94ff3a152b1a3d944660f9ff7d49bc
Variant25 <ayushp041@gmail.com>
2024-08-07 01:57:17 +0530

    header updated

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -15,17 +15,17 @@ function Header() {
     <div className='flex p-4 items-center justify-between bg-slate-100 shadow-sm'>
         <Image src={'/logo.svg'} width={160} height={100} alt='logo'/>
         <ul className='hidden md:flex gap-6'>
-            <li className={`hover:text-secondary hover:font-bold transition-all cursor-pointer
-               ${path=='/dashboard'&&'text-secondary font-bold'}`
+            <li className={`hover:text-primary  hover:font-bold transition-all cursor-pointer
+               ${path=='/dashboard'&&'text-primary font-bold'}`
               }>Dashboard</li>
-            <li className={`hover:text-secondary hover:font-bold transition-all cursor-pointer
-               ${path=='/dashboard/questions'&&'text-secondary font-bold'}`
+            <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer
+               ${path=='/dashboard/questions'&&'text-primary font-bold'}`
               }>Questions</li>
-            <li className={`hover:text-secondary hover:font-bold transition-all cursor-pointer
-               ${path=='/dashboard/upgrade'&&'text-secondary font-bold'}`
+            <li className={`hover:text-primary  hover:font-bold transition-all cursor-pointer
+               ${path=='/dashboard/upgrade'&&'text-primary  font-bold'}`
               }>Upgrade</li>
-            <li className={`hover:text-secondary hover:font-bold transition-all cursor-pointer
-               ${path=='/dashboard/how'&&'text-secondary font-bold'}`
+            <li className={`hover:text-primary  hover:font-bold transition-all cursor-pointer
+               ${path=='/dashboard/how'&&'text-primary  font-bold'}`
               }>How it works?</li>
         </ul>
         <UserButton/>
